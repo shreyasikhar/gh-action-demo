728275c41e820aa98e88f05775c3d6d80b8235e2
Shreyas Ikhar <shreyasikhar26@gmail.com>
2022-10-30 14:30:36 +0530

    Create GH action which uses default and custom env variables, and reads file content

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hey There, This is first message executed from GH actions"
+echo "##### Listing Github repository content #####"
+ls $GITHUB_WORKSPACE
+
+echo "##### cat fact.txt from GitHub repository #####"
+cat $GITHUB_WORKSPACE/fact.txt
+
+echo "##### Example of environment variables #####"
+echo "Hey I am $user_name and my WordPress.org user profile is https://profiles.wordpress.org/shreyasikhar26/"
